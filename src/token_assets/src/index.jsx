9a488997486710e33959bf508cbcc804747bd1e7
kyawhtay <kyaw.htay@gmail.com>
2024-03-17 14:47:52 +0000

    Added Auth

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,8 +1,26 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from '@dfinity/auth-client'
 
 const init = async () => { 
+  const authClient = await AuthClient.create();
+
+  if(await authClient.isAuthenticated()){
+    handleAuthentication(authClient);
+  }
+  else{
+
+  await authClient.login({
+    identityProvider: "https://identity.ic0.app/#authorize",
+    onSuccess:()=>{
+      handleAuthentication(authClient);
+    }
+  })
+ }
+}
+
+async function handleAuthentication(authClient){
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
